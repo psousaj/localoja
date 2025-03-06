c38e689b63842ccb8d08d84a36cd0dcb9f95c8ca
José Pinheiro <josepsousa2012@gmail.com>
2025-03-05 23:15:31 -0300

    fix: resolve conflitos no middleware e adiciona variável de ambiente para chave da API do Google Maps

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -6,6 +6,7 @@ const envSchema = z.object({
     PGUSER: z.string({ message: "Provides PGUSER env. *hint: check .env file for set" }),
     PGPASSWORD: z.string({ message: "Provides PGPASSWORD env. *hint: check .env file for set" }),
     SESSION_SECRET: z.string().min(32, "SESSION_SECRET should have at least 32 characters"),
+    GMAPS_GEOCODING_APIKEY: z.string({ message: "Provides GMAPS_GEOCODING_APIKEY env. *hint: check .env file for set" }),
     PORT: z.coerce.number().default(3003),
     HOST: z.string().default('0.0.0.0'),
     NODE_ENV: z.enum(['development', 'test', 'production']).default('development'),
