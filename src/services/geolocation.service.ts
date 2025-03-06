b4a8f43d5b4b6a9aa82c3e3b15bd309f62423575
José Pinheiro <josepsousa2012@gmail.com>
2025-03-06 00:31:38 -0300

    feat: adiciona serviço de geolocalização com métodos para busca por endereço e CEP

---
+++ src/services/geolocation.service.ts
@@ -0,0 +1,20 @@
+import { env } from "../config/env";
+
+export class GeolocationAPI {
+    /**
+     * * REFERENCES
+     * * https://developers.google.com/maps/documentation/geocoding/requests-geocoding?hl=pt-br#geocoding-lookup
+     * * https://stackoverflow.com/questions/65041545/how-can-i-use-longitude-and-latitude-with-typeorm-and-postgres
+     **/
+
+
+    static async getGeoLocationByAddress(address: string) {
+        const place = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${env.GMAPS_GEOCODING_APIKEY}`).then((res) => res.json());
+        return place;
+    }
+
+    static async getPlaceByCep(cep: string) {
+        const place = await fetch(`https://viacep.com.br/ws/${cep}/json/`).then((res) => res.json());
+        return place;
+    }
+}
