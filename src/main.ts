12c625a790d7335fe4a6a3bb75e85defc20162ae
José Pinheiro <josepsousa2012@gmail.com>
2025-04-12 17:43:27 -0300

    feat: adiciona suporte a sessões com express-session e define tipos personalizados

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as session from 'express-session';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  });
+
+  app.use(session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+    // store
+  }))
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
